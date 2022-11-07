9871938170c745f1920ad16caba50f55ff734dc2
Cardinum <pmgnarong@gmail.com>
2022-11-08 08:20:25 +0900

    수능 끝 조건 추가

--- js/SATcount spent.js
+++ js/SATcount spent.js
@@ -1,18 +1,31 @@
 const Start = '13 dec 2021 07:40:00 GMT+0900'
-
+import { Flip } from 'number-flip'
 function formatTime(time) {
     return time < 10 ? '0' + time : time;
 }
 
+
+
 function countdown() {
     const StartDate = new Date(Start);
     const currentDate = new Date();
+    const SAT = new Date('18 nov 2022 08:40:00 GMT+0900');
+    if (currentDate >= SAT) {
+        const totSec = (SAT - StartDate) / 1000;
+        const days = Math.floor(totSec / 3600 / 24);
+        const hours = (days * 24) + Math.floor(totSec / 3600) % 24;
+        const min = Math.floor(totSec / 60) % 60;
+        const sec = Math.floor(totSec) % 60;
+    }
+    else {
+        const totSec = (currentDate - StartDate) / 1000;
+        const days = Math.floor(totSec / 3600 / 24);
+        const hours = (days * 24) + Math.floor(totSec / 3600) % 24;
+        const min = Math.floor(totSec / 60) % 60;
+        const sec = Math.floor(totSec) % 60;
+    }
 
-    const totSec = (currentDate - StartDate) / 1000;
-    const days = Math.floor(totSec / 3600 / 24);
-    const hours = (days * 24) + Math.floor(totSec / 3600) % 24;
-    const min = Math.floor(totSec / 60) % 60;
-    const sec = Math.floor(totSec) % 60;
+    
 
     document.getElementById('hours_spent').innerText = formatTime(hours);
     document.getElementById('min_spent').innerText = formatTime(min);
--- js/SATcount.js
+++ js/SATcount.js
@@ -1,3 +1,4 @@
+import { Flip } from 'number-flip'
 const SAT = '18 nov 2022 08:40:00 GMT+0900'
 
 function formatTime(time) {
@@ -7,16 +8,27 @@ function formatTime(time) {
 function countdown() {
     const SATDate = new Date(SAT);
     const currentDate = new Date();
+    if (currentDate >= SATDate) {
+        document.getElementById('hours').innerText = "수";
+        document.getElementById('min').innerText = "능";
+        document.getElementById('sec').innerText = "끝";
+    }
+    else {
+        const totSec = (SATDate - currentDate) / 1000;
+        const days = Math.floor(totSec / 3600 / 24);
+        const hours = (days * 24) + Math.floor(totSec / 3600) % 24;
+        const min = Math.floor(totSec / 60) % 60;
+        const sec = Math.floor(totSec) % 60;
 
-    const totSec = (SATDate - currentDate) / 1000;
-    const days = Math.floor(totSec / 3600 / 24);
-    const hours = (days * 24) + Math.floor(totSec / 3600) % 24;
-    const min = Math.floor(totSec / 60) % 60;
-    const sec = Math.floor(totSec) % 60;
+        document.getElementById('hours').innerText = formatTime(hours);
+        document.getElementById('min').innerText = formatTime(min);
+        document.getElementById('sec').innerText = formatTime(sec);
+    }
+    
 
-    document.getElementById('hours').innerText = formatTime(hours);
-    document.getElementById('min').innerText = formatTime(min);
-    document.getElementById('sec').innerText = formatTime(sec);
+    
 }
 
 setInterval(countdown,1000)
+
+
